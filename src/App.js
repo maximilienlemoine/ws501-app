6135379cec39ede9590244db2d2ed17d37569df1
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-03-18 12:27:12 +0100

    feat : import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import {GLTFLoader} from "three/addons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const loader = new GLTFLoader()
+    const [file, setFile] = useState();
+    const [gltf, setGltf] = useState();
+
+    function handleChange(event) {
+        setFile(event.target.files[0])
+    }
+
+    function onSubmit() {
+        const url = URL.createObjectURL(file);
+        loader.load(
+            url,
+            (gltf) => {
+                setGltf(gltf)
+                console.log('success', gltf)
+            },
+            (progress) => {
+                console.log('progress', progress)
+            },
+            (error) => {
+                console.log('error', error)
+            }
+        )
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {gltf && (
+                    <section>
+                        <h2>Modèle 3D chargé</h2>
+                            <li>Nom: {gltf.scene.name}</li>
+                            {gltf.scene.children.map((child, index) => (
+                                <div key={index}>Elément {index}: {child.name}
+                                </div>
+                            ))}
+                    </section>
+                )}
+                {!gltf && (
+                    <section>
+                        <h2>Charger un modèle 3D</h2>
+                        <input type="file" onChange={handleChange}/>
+                        {file && (
+                            <section>
+                                Détails du modèle 3D:
+                                <ul>
+                                    <li>Nom: {file.name}</li>
+                                    <li>Poids: {file.size} bytes</li>
+                                </ul>
+                            </section>
+                        )}
+                        <button onClick={onSubmit}>Submit</button>
+                    </section>
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
